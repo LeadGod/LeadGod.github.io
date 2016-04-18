5c15064a9da4633c0e0e0bcf33e53cba83dd5f60
SimonNguyen <damnfann@gmail.com>
2016-04-18 03:41:12 -0500

    Removed makeUL

--- js/main.js
+++ js/main.js
@@ -12,16 +12,4 @@ for( var i = 0; i < data.length; i++ )
     ul.appendChild(li);
 }
 
-function makeUL(array) {
-    var list = document.createElement('ul');
-    for(var i = 0; i < array.length; i++) {
-        var item = document.createElement('li');
-        item.appendChild(document.createTextNode(array[i]));
-        list.appendChild(item);
-    }
-
-
-  return list;
-  }
-
 });
