915fa1efd9926d0dc7dd6909ae2c4997d1362c25
SimonNguyen <damnfann@gmail.com>
2016-04-18 03:26:59 -0500

    JSON implementation of populating guns

--- js/main.js
+++ js/main.js
@@ -1,3 +1,27 @@
-// Custom separator in csv file; Do this for guns and items
-var options={"separator" : "|"};
-$.csv.toArrays(csv, options);
+$.getJSON('guns.json', function (data) {
+var o = null;
+var ul = document.getElementById("guns");
+var myArray = new Array();
+document.open();
+
+for( var i = 0; i < data.length; i++ )
+{
+    o = data[i];
+    var li = document.createElement("li");
+    li.appendChild(document.createTextNode(o.Name));
+    ul.appendChild(li);
+}
+
+function makeUL(array) {
+    var list = document.createElement('ul');
+    for(var i = 0; i < array.length; i++) {
+        var item = document.createElement('li');
+        item.appendChild(document.createTextNode(array[i]));
+        list.appendChild(item);
+    }
+
+
+  return list;
+  }
+
+});
