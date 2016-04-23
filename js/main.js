41c3a777a47e220aa364e5a83f37fe13add957c4
Simon Nguyen <Simon Nguyen>
2016-04-23 15:31:22 -0500

    Signed-off-by: Simon Nguyen <Simon Nguyen>

--- js/main.js
+++ js/main.js
@@ -16,3 +16,22 @@ for( var i = 0; i < data.length; i++ )
 }
 
 });
+
+$.getJSON('items.json', function (data) {
+var o = null;
+var ul = document.getElementById("items");
+console.dir(ul);
+var myArray = new Array();
+
+for( var i = 0; i < data.length; i++ )
+{
+    o = data[i];
+    var li = document.createElement("li");
+	var img = document.createElement("img");
+	img.setAttribute("src", "/img/items/" + o.Icon);
+	img.setAttribute("alt", o.Name);
+    li.appendChild(img);
+    ul.appendChild(li);
+}
+
+});
