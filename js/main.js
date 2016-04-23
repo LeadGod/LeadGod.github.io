6c1b4f2b498aef079640d38f7e67f6466c339945
Simon Nguyen <Simon Nguyen>
2016-04-23 15:37:05 -0500

    Signed-off-by: Simon Nguyen <Simon Nguyen>

--- js/main.js
+++ js/main.js
@@ -1,37 +1,31 @@
 $.getJSON('guns.json', function (data) {
 var o = null;
-var ul = document.getElementById("guns");
-console.dir(ul);
+var p = document.getElementById("guns");
 var myArray = new Array();
 
 for( var i = 0; i < data.length; i++ )
 {
     o = data[i];
-    var li = document.createElement("li");
 	var img = document.createElement("img");
 	img.setAttribute("src", "/img/guns/" + o.Icon);
 	img.setAttribute("alt", o.Name);
-    li.appendChild(img);
-    ul.appendChild(li);
+    p.appendChild(img);
 }
 
 });
 
 $.getJSON('items.json', function (data) {
 var o = null;
-var ul = document.getElementById("items");
-console.dir(ul);
+var p = document.getElementById("items");
 var myArray = new Array();
 
 for( var i = 0; i < data.length; i++ )
 {
     o = data[i];
-    var li = document.createElement("li");
 	var img = document.createElement("img");
 	img.setAttribute("src", "/img/items/" + o.Icon);
 	img.setAttribute("alt", o.Name);
-    li.appendChild(img);
-    ul.appendChild(li);
+    p.appendChild(img);
 }
 
 });
