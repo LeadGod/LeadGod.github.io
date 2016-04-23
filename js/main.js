bf18cccd62e19d28ca482972cec533a38f89bb9d
Simon Nguyen <Simon Nguyen>
2016-04-23 01:39:32 -0500

    Signed-off-by: Simon Nguyen <Simon Nguyen>

--- js/main.js
+++ js/main.js
@@ -3,13 +3,12 @@ var o = null;
 var ul = document.getElementById("guns");
 console.dir(ul);
 var myArray = new Array();
-// document.open();
 
 for( var i = 0; i < data.length; i++ )
 {
     o = data[i];
     var li = document.createElement("li");
-    li.appendChild(document.createTextNode(o.Name));
+    li.appendChild(document.createTextNode('<img src="/img/guns/"' + o.Icon + '>'));
     ul.appendChild(li);
 }
 
