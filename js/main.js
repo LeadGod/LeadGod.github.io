0dd8112ba2c643d1ad3d953a12da93e8ec0554eb
bbeshel <bbeshel@slu.edu>
2016-04-24 20:35:10 -0400

    Converted DOM manipulation to jQuery
    Added div rows of 10 spans to hold self resizing gameObject images
    
    Added 3 CSS classes to setup resizing
    
    Added a document.ready to wait for all scripts to load before main loads

--- js/main.js
+++ js/main.js
@@ -1,35 +1,65 @@
-$.getJSON('guns.json', function (data) {
-var o = null;
-var p = document.getElementById("guns");
-var myArray = new Array();
 
-for( var i = 0; i < data.length; i++ )
-{
-    o = data[i];
-	var img = document.createElement("img");
-	img.setAttribute("src", "/img/guns/" + o.Icon);
-	img.setAttribute("alt", o.Name);
-	img.setAttribute("title", o.Name);
-	img.setAttribute("class", "gameObject");
-    p.appendChild(img);
-}
+$(document).ready(function () {
+	var ROW_LENGTH = 10;
+	
+	$.getJSON('guns.json', function (data) {
+	var object = null;
+	// var p = document.getElementById("guns");
+	var $section = $("#guns");
+	var $div = $('<div class="iconRow">');
+	var $span = $('<span class="iconContainer">');
 
-});
+	for( var i = 0; i < data.length; i++ )
+	{
+		$span = $('<span class="iconContainer">');
+		
+		if ((i + 2) % ROW_LENGTH === 0) {
+			$div = $('<div class="iconRow">');
+			$section.append($div);
+		}
+		
+		object = data[i];
+		var $img = ('<img alt="' + object.Name + '" ' +
+			'class="gameObject"' +
+			'src="/img/guns/' + object.Icon + '">'
+		);
+		$span.append($img);
+		$div.append($span);
+		
+		// $img.attr("src", "/img/guns/" + object.Icon);
+		// $img.attr("alt", object.Name);
+		// $img.attr("id", "gameObject");
+	}
 
-$.getJSON('items.json', function (data) {
-var o = null;
-var p = document.getElementById("items");
-var myArray = new Array();
+	});
 
-for( var i = 0; i < data.length; i++ )
-{
-    o = data[i];
-	var img = document.createElement("img");
-	img.setAttribute("src", "/img/items/" + o.Icon);
-	img.setAttribute("alt", o.Name);
-	img.setAttribute("title", o.Name);
-	img.setAttribute("class", "gameObject")
-    p.appendChild(img);
-}
+	$.getJSON('items.json', function (data) {
+	var object = null;
+	// var p = document.getElementById("items");
+	var $section = $("#items");
+	var $div = $('<div class="iconRow">');
+	var $span = $('<span class="iconContainer">');
 
+	for( var i = 0; i < data.length; i++ )
+	{
+		$span = $('<span class="iconContainer">');
+		
+		if ((i + 2) % ROW_LENGTH === 0) {
+			$div = $('<div class="iconRow">');
+			$section.append($div);
+		}
+		
+		object = data[i];
+		var $img = ('<img alt="' + object.Name + '" ' +
+			'class="gameObject"' +
+			'src="/img/items/' + object.Icon + '">'
+		);
+		$span.append($img);
+		$div.append($span);
+		
+		
+		
+	}
+
+	});
 });
