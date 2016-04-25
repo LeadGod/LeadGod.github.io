88250e1f11aa6da31adbc406e4013cd8e16cd41a
SimonNguyen <damnfann@gmail.com>
2016-04-24 22:57:09 -0500

    Minor display changes and added image hover text.

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 
 $(document).ready(function () {
 	var ROW_LENGTH = 10;
-	
+
 	$.getJSON('guns.json', function (data) {
 	var object = null;
 	// var p = document.getElementById("guns");
@@ -12,23 +12,20 @@ $(document).ready(function () {
 	for( var i = 0; i < data.length; i++ )
 	{
 		$span = $('<span class="iconContainer">');
-		
+
 		if ((i + 2) % ROW_LENGTH === 0) {
 			$div = $('<div class="iconRow">');
 			$section.append($div);
 		}
-		
+
 		object = data[i];
 		var $img = ('<img alt="' + object.Name + '" ' +
+            'title="' + object.Name + '" ' +
 			'class="gameObject"' +
 			'src="/img/guns/' + object.Icon + '">'
 		);
 		$span.append($img);
 		$div.append($span);
-		
-		// $img.attr("src", "/img/guns/" + object.Icon);
-		// $img.attr("alt", object.Name);
-		// $img.attr("id", "gameObject");
 	}
 
 	});
@@ -43,22 +40,20 @@ $(document).ready(function () {
 	for( var i = 0; i < data.length; i++ )
 	{
 		$span = $('<span class="iconContainer">');
-		
+
 		if ((i + 2) % ROW_LENGTH === 0) {
 			$div = $('<div class="iconRow">');
 			$section.append($div);
 		}
-		
+
 		object = data[i];
 		var $img = ('<img alt="' + object.Name + '" ' +
+            'title="' + object.Name + '" ' +
 			'class="gameObject"' +
 			'src="/img/items/' + object.Icon + '">'
 		);
 		$span.append($img);
 		$div.append($span);
-		
-		
-		
 	}
 
 	});
