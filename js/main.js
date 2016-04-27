2664284362f9e09171ab2dd60406a3120040f0d1
SimonNguyen <damnfann@gmail.com>
2016-04-27 13:10:48 -0500

    Properly created objects

--- js/main.js
+++ js/main.js
@@ -9,6 +9,7 @@ $(document).ready(function () {
     }
 
     var content = {
+
     }
 
 	$.getJSON('guns.json', function (data) {
@@ -34,23 +35,23 @@ $(document).ready(function () {
 		);
 
         var $a = $('<a href="#" id="' + object.Name +
-                '" class="showInfo "' + object.Name + '""); return false;">');
+                '" class="showInfo ' + object.Name + '"');
 
-        var itemHTML = $();
-        var itemName = $('<h2>' + object.Name + '</h2>');
-        var itemQuote = $('<p>"' + object.Quote + '"</p>');
-        var itemType = $('<ul><li>' + object.Type + '</li>');
-        var itemClip = $('<li>Clip Size: ' + object["Clip Size"] + '</li>');
-        var itemAmmo = $('<li>Ammo: ' + object.Ammo + '</li>');
-        var itemNotes = $('<li>Notes: ' + object.Notes + '</li></ul>');
-        itemHTML.append(itemName);
-        itemHTML.append(itemQuote);
-        itemHTML.append(itemType);
-        itemHTML.append(itemClip);
-        itemHTML.append(itemAmmo);
-        itemHTML.append(itemNotes);
+        var $itemHTML = $();
+        var $itemName = $('<h2>' + object.Name + '</h2>');
+        var $itemQuote = $('<p>"' + object.Quote + '"</p>');
+        var $itemType = $('<ul><li>' + object.Type + '</li>');
+        var $itemClip = $('<li>Clip Size: ' + object["Clip Size"] + '</li>');
+        var $itemAmmo = $('<li>Ammo: ' + object.Ammo + '</li>');
+        var $itemNotes = $('<li>Notes: ' + object.Notes + '</li></ul>');
+        $itemHTML.append($itemName);
+        $itemHTML.append($itemQuote);
+        $itemHTML.append($itemType);
+        $itemHTML.append($itemClip);
+        $itemHTML.append($itemAmmo);
+        $itemHTML.append($itemNotes);
 
-        content[object.Name] = itemHTML;
+        content[object.Name] = $itemHTML;
         $a.append($img);
 		$span.append($a);
 		$div.append($span);
@@ -81,19 +82,19 @@ $(document).ready(function () {
 		);
 
         var $a = $('<a href="#" id="' + object.Name +
-                '" onclick="updateText("' + object.Name + '""); return false;">');
+                '" class="showInfo ' + object.Name + '"');
 
-        var itemHTML = $();
-        var itemName = $('<h2>' + object.Name + '</h2>');
-        var itemQuote = $('<p>"' + object.Quote + '"</p>');
-        var itemType = $('<ul><li>' + object.Type + '</li>');
-        var itemEffect = $('<li>Notes: ' + object.Effect + '</li></ul>');
-        itemHTML.append(itemName);
-        itemHTML.append(itemQuote);
-        itemHTML.append(itemType);
-        itemHTML.append(itemEffect);
+        var $itemHTML = $();
+        var $itemName = $('<h2>' + object.Name + '</h2>');
+        var $itemQuote = $('<p>"' + object.Quote + '"</p>');
+        var $itemType = $('<ul><li>' + object.Type + '</li>');
+        var $itemEffect = $('<li>Notes: ' + object.Effect + '</li></ul>');
+        $itemHTML.append($itemName);
+        $itemHTML.append($itemQuote);
+        $itemHTML.append($itemType);
+        $itemHTML.append($itemEffect);
 
-        content[object.Name] = itemHTML;
+        content[object.Name] = $itemHTML;
         $a.append($img);
         $span.append($a);
         $div.append($span);
