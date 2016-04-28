c047f6af72f0e18a5bbc68c48390e086b1260f14
SimonNguyen <damnfann@gmail.com>
2016-04-27 20:00:11 -0500

    Testing grid changes

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ $(window).load(function() {
     {
         srcNode: 'img',             // grid items (class, node)
         margin: '5px',             // margin in pixel, default: 0px
-        width: '30px',             // grid item width in pixel, default: 220px
+        width: '35px',             // grid item width in pixel, default: 220px
         max_width: '',              // dynamic gird item width if specified, (pixel)
         resizable: true,           // re-layout if window resize
         transition: 'all 0.5s ease' // support transition for CSS3, default: all 0.5s ease
