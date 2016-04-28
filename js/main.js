8c8832dfb4bc6fa1a7dd1e1d6bf03f3e405af851
SimonNguyen <damnfann@gmail.com>
2016-04-28 12:33:28 -0500

    Signed-off-by: SimonNguyen <damnfann@gmail.com>

--- js/main.js
+++ js/main.js
@@ -56,8 +56,8 @@ $(window).load(function () {
 		var $img = ('<img alt="' + object.Name + '" ' +
 					'title="' + object.Name + '" ' +
 					'class="gameObject gameObject-' + res +
-					'" src="/img/trans.png>'
-		);
+					'" src="/img/trans.png">'
+					);
 		var $a = $('<a href="#' + object.Name +
 				'" class="showInfo" data-loc="' + object.Name + '">');
 
@@ -104,8 +104,8 @@ $(window).load(function () {
 		var $img = ('<img alt="' + object.Name + '" ' +
 					'title="' + object.Name + '" ' +
 					'class="gameObject gameObject-' + res +
-					'" src="/img/trans.png>'
-		);
+					'" src="/img/trans.png">'
+					);
 		var $a = $('<a href="#' + object.Name +
 				'" class="showInfo" data-loc="' + object.Name + '">');
 
